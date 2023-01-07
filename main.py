536fa699d33d38e6d3649fd6baf7c0164caa7926
vcern <vladiiislaw64@gmail.com>
2023-01-07 13:36:58 +0300

    Added Menu

--- main.py
+++ main.py
@@ -1,10 +1,43 @@
+from kivy.lang import Builder
+
+from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.app import MDApp
-from kivymd.uix.label import MDLabel
+
+KV = '''
+MDScreen:
+
+    MDNavigationLayout:
+
+        MDScreenManager:
+
+            MDScreen:
+
+                MDTopAppBar:
+                    title: "Navigation Drawer"
+                    elevation: 4
+                    pos_hint: {"top": 1}
+                    md_bg_color: "#2e7d32"
+                    specific_text_color: "#FFFFFF"
+                    left_action_items:
+                        [['menu', lambda x: nav_drawer.set_state("open")]]
 
 
-class MortgageCalculatorApp(MDApp):
+        MDNavigationDrawer:
+            id: nav_drawer
+            radius: (0, 16, 16, 0)
+
+            ContentNavigationDrawer:
+'''
+
+
+class ContentNavigationDrawer(MDBoxLayout):
+    pass
+
+
+class MortgageCalculator(MDApp):
     def build(self):
-        return MDLabel(text="Hello, World", halign="center")
+        self.theme_cls.theme_style = "Light"
+        return Builder.load_string(KV)
 
 
-MortgageCalculatorApp().run()
+MortgageCalculator().run()
